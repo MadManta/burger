9c1e2adc2efc35b9bc07c69fc64572a452d56b21
Sammy Straw <fruitbatcloud@gmail.com>
2018-05-10 10:50:05 -0600

    trying to fix db for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,6 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    port: 3306,
     host: "us-cdbr-iron-east-04.cleardb.net",
     user: "b4b56f91f784bc",
     password: "db8ab2fe",
@@ -17,5 +16,3 @@ connection.connect(function(err) {
 });
 
 module.exports = connection;
-
-heroku_19ea29ec73b1ef5
