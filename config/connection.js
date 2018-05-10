96ac5f0c6c6ad721c6e5fb17451d4b9b54c55f23
Sammy Straw <fruitbatcloud@gmail.com>
2018-05-10 10:38:38 -0600

    changing DB

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
     port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "catbeans",
-    database: "burgers_db"
+    host: "us-cdbr-iron-east-04.cleardb.net",
+    user: "b4b56f91f784bc",
+    password: "db8ab2fe",
+    database: "heroku_19ea29ec73b1ef5"
 });
 
 connection.connect(function(err) {
@@ -17,3 +17,5 @@ connection.connect(function(err) {
 });
 
 module.exports = connection;
+
+heroku_19ea29ec73b1ef5
